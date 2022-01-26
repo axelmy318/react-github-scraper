f552cb820cb0074457e3a21b53f33caa99ae8662
axel_milliery <axel.milliery@logiccenter.ch>
2022-01-26 16:43:54 +0100

    "PushedAt" date is now converted to a more readable format

--- src/components/PushedAt/PushedAt.js
+++ src/components/PushedAt/PushedAt.js
@@ -15,10 +15,15 @@ const PushedAt = ({ prefix, label }) => {
         setContentKey(CONTENT_KEY, {...response})
     }
 
+    const convertDate = (date) => {
+        let d = new Date(date)
+        return d.toLocaleString()
+    }
+
     return (<div className='github-scraper-component component-pushedat'>
         {content[CONTENT_KEY] !== null && content[CONTENT_KEY].success && <>
             {label && <div className='github-scraper-component-label'>{label}</div>}
-            <div className='github-scraper-component-content' style={{fontSize: '130%'}}>{prefix && prefix}{content[CONTENT_KEY].data.pushed_at}</div>
+            <div className='github-scraper-component-content' style={{fontSize: '130%'}}>{prefix && prefix}{convertDate(content[CONTENT_KEY].data.pushed_at)}</div>
         </>}
     </div>)
 }
