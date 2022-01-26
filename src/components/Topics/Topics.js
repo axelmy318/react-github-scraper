42c6e7242cb3abd1518beb76296d4f477b4c844e
Axel Milliery <a.milliery@icloud.com>
2022-01-26 20:22:45 +0100

    Updated topics so that the user can click on it

--- src/components/Topics/Topics.js
+++ src/components/Topics/Topics.js
@@ -15,12 +15,16 @@ const Topics = ({ label }) => {
         setContentKey(CONTENT_KEY, {...response})
     }
 
+    const sendToTopic = topic => {
+        window.open(`https://www.github.com/topics/${topic}`, '_blank').focus();
+    }
+
     return (<div className='github-scraper-component component-topics'>
         {content[CONTENT_KEY] !== null && content[CONTENT_KEY].success && <>
             {label && <div className='github-scraper-component-label'>{label}</div>}
             <div className='github-scraper-component-content' style={{fontSize: '130%'}}>
                 { content[CONTENT_KEY].data.topics.length > 0
-                ? content[CONTENT_KEY].data.topics.map(topic => <span className='github-scraper-topic'>{topic}</span>)
+                ? content[CONTENT_KEY].data.topics.map(topic => <span className='github-scraper-topic clickable' onClick={() => sendToTopic(topic)}>{topic}</span>)
                 : <i>This repository has no topics</i>
             }
             </div>
